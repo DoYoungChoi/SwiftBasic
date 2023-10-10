8153334923fd3c3d4a85a41e0406e99398d6021f
dodor <choidy9298@naver.com>
2023-10-10 16:12:21 +0900

    Part1. Swift 기초 문법 3-4. 열거형 - 필요한 선택지를 만드는 방법

---
+++ SwiftBasic/Ch3.코드로구조잡기/Enumeration.swift
@@ -0,0 +1,37 @@
+//
+//  Enumeration.swift
+//  SwiftBasic
+//
+//  Created by dodor on 2023/10/10.
+//
+
+import SwiftUI
+
+struct Enumeration: View {
+    
+    var direction: Direction = .north
+    var menu: Menu = .steak("rare")
+    
+    var body: some View {
+        Text("방향은 \(direction.rawValue)쪽 입니다")
+    }
+}
+
+enum Direction: String {
+    case north = "북"
+    case sorth = "남"
+    case east = "동"
+    case west = "서"
+}
+
+enum Menu {
+    case pasta
+    case pizza
+    case steak(String)
+}
+
+struct Enumeration_Previews: PreviewProvider {
+    static var previews: some View {
+        Enumeration()
+    }
+}
