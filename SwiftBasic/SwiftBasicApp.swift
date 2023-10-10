2dde662ee8c13660aef825aa11dfdcc72666b6eb
dodor <choidy9298@naver.com>
2023-10-10 12:41:43 +0900

    Initial Commit

---
+++ SwiftBasic/SwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBasicApp.swift
+//  SwiftBasic
+//
+//  Created by dodor on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
