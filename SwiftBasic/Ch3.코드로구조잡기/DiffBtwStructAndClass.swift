17604eb6eea2316478e56088f8f7c5ad1e13feac
dodor <choidy9298@naver.com>
2023-10-10 15:39:54 +0900

    Part1. Swift 기초 문법 3-2. 클래스 - 주소값으로 객체를 만들기

---
+++ SwiftBasic/Ch3.코드로구조잡기/DiffBtwStructAndClass.swift
@@ -0,0 +1,75 @@
+//
+//  DiffBtwStructAndClass.swift
+//  SwiftBasic
+//
+//  Created by dodor on 2023/10/10.
+//
+
+import SwiftUI
+
+struct DiffBtwStructAndClass: View {
+    
+    let myCar = Car(name: "도돌카", owner: "도돌")
+    @State private var greetingCarOwner: String = ""
+    let myKar = Kar(name: "라떼카", owner: "라떼")
+    @State private var greetingKarOwner: String = ""
+    
+    var body: some View {
+        VStack {
+            Text("\(myCar.name)의 주인은 \(myCar.owner) 입니다.")
+            
+            Text(greetingCarOwner)
+            Button {
+                var sisCar = myCar
+                sisCar.name = "여동생차"
+                sisCar.owner = "여동생"
+                
+                greetingCarOwner = myCar.sayHi()
+            } label: {
+                Text("Car 주인에게 인사하기")
+            }
+            
+            Text("\(myKar.name)의 주인은 \(myKar.owner) 입니다.")
+            
+            Text(greetingKarOwner)
+            Button {
+                let broKar = myKar
+                broKar.name = "남동생차"
+                broKar.owner = "남동생"
+                
+                greetingKarOwner = myKar.sayHi()
+            } label: {
+                Text("Kar 주인에게 인사하기")
+            }
+        }
+    }
+}
+
+struct Car {
+    var name: String
+    var owner: String
+    
+    func sayHi() -> String {
+        return "안녕하세요! \(owner)님"
+    }
+}
+
+class Kar {
+    var name: String
+    var owner: String
+    
+    init(name: String, owner: String) {
+        self.name = name
+        self.owner = owner
+    }
+    
+    func sayHi() -> String {
+        return "안녕하세요! \(owner)님"
+    }
+}
+
+struct DiffBtwStructAndClass_Previews: PreviewProvider {
+    static var previews: some View {
+        DiffBtwStructAndClass()
+    }
+}
