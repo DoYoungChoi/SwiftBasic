a12ec7923110bef78fdcf73761c4515c2f9723c0
dodor <choidy9298@naver.com>
2023-10-10 13:59:02 +0900

    Part1. Swift 기초 문법 2-4. 조건문 - 조건에 따라 다르게 동작하는 방법

---
+++ SwiftBasic/Ch2.문법의첫걸음/Condition.swift
@@ -0,0 +1,37 @@
+//
+//  Condition.swift
+//  SwiftBasic
+//
+//  Created by dodor on 2023/10/10.
+//
+
+import SwiftUI
+
+struct Condition: View {
+    
+    var count = 4
+    var hasLoggedIn = false
+    
+    var body: some View {
+        VStack {
+            if count > 4 {
+                Text("숫자는 4보다 큽니다.")
+            } else {
+                Text("숫자는 4와 같거나 작습니다.")
+            }
+            
+            
+//            guard hasLoggedIn else {
+//                Text("로그인 하시겠습니까?")
+//            }
+//            
+//            Text("로그아웃 하시겠습니까?")
+        }
+    }
+}
+
+struct Condition_Previews: PreviewProvider {
+    static var previews: some View {
+        Condition()
+    }
+}
