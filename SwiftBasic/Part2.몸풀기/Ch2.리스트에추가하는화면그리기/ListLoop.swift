9951cd3adeb7b1ed305e8a083607a50e8f588963
dodor <choidy9298@naver.com>
2023-10-11 13:33:31 +0900

    Part2. SwiftUI 기초 2-2. 데이터 모델링

--- SwiftBasic/Part2.몸풀기/Ch2.리스트에추가하는화면그리기/ListLoop.swift
+++ SwiftBasic/Part2.몸풀기/Ch2.리스트에추가하는화면그리기/ListLoop.swift
@@ -7,16 +7,41 @@
 
 import SwiftUI
 
+struct Fruit: Identifiable {
+    let id = UUID()
+    let name: String
+    let matchFruitName: String
+    let price: Int
+}
+
 struct ListLoop: View {
     
-    var fruits: [String] = ["Apple", "Banana", "Cherry", "Double Kiwi", "Elder berry"]
+    var fruits = [
+        Fruit(name: "Apple",
+              matchFruitName: "Banana",
+              price: 1000),
+        Fruit(name: "Banana",
+              matchFruitName: "Banana",
+              price: 3000),
+        Fruit(name: "Cherry",
+              matchFruitName: "Banana",
+              price: 4000),
+        Fruit(name: "Double Kiwi",
+              matchFruitName: "Elder berry",
+              price: 2400),
+        Fruit(name: "Elder berry",
+              matchFruitName: "Double Kiwi",
+              price: 8000)
+    ]
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(fruits, id:\.self) { fruit in
+                ForEach(fruits) { fruit in
                     HStack {
-                        Text(fruit)
+                        Text(fruit.name)
+                        Spacer()
+                        Text("\(fruit.price)")
                     }
                 }
             }
